c0645f76da90da32437e4692c1b2c017d9542ce7
TasyDev <tamayocontat@gmail.com>
2026-02-23 02:58:15 -0500

    feat: Comida model

---
+++ src/domain/models/comida.py
@@ -0,0 +1,13 @@
+from Producto import Producto
+
+class Comida(Producto):
+    def __init__(self, nombre: str, precio: float, descripcion: str, disponible: bool, categoria: str, calorias: int):
+        super().__init__(nombre, precio, descripcion, disponible)
+        self.categoria = categoria
+        self.calorias = calorias
+    
+    def haciendoComida(self):
+        print(f"Haciendo {self.nombre}...")
+        print(f"Categoria: {self.categoria}")
+        print(f"Calorias: {self.calorias}")
+        print("Listo!")
